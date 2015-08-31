3d0306d5ebbc71d504338283edcd2e074103f015
Ron Lin <ronrlin@gmail.com>
2015-08-31 16:17:03 -0700

    support for naive bayesian document classifiers

--- classifier.py
+++ classifier.py
@@ -46,7 +46,7 @@ class AgreementNaiveBayesClassifier(object):
 	def __init__(self, corpus):
 		self.corpus = corpus
 		self.classifier = None
-		self.words_ds = nltk.FreqDist(words.lower() for words in self.corpus.words())
+		self.words_ds = nltk.FreqDist(words.lower().rstrip('*_-. ') for words in self.corpus.words())
 
 	def fit(self):
 		""" """
@@ -57,7 +57,10 @@ class AgreementNaiveBayesClassifier(object):
 		self.classifier = nltk.NaiveBayesClassifier.train(train_set)
 
 	def get_features(self, doc):
-		""" """
+		""" 
+		Returns the features for this document.
+		:param doc: list of strings 
+		"""
 		doc_words = set(doc)
 		word_features = list(self.words_ds)[:3000]
 		features = {}
@@ -65,24 +68,38 @@ class AgreementNaiveBayesClassifier(object):
 			features['contains(%s)' % word] = (word in doc_words)
 		return features
 
-	def classify_file(self):
-		""" """
-		pass
+	def classify_data(self, data):
+		""" 
+		:param data: list of strings
+		"""
+		return self.classifier.classify(self.get_features(data))
 
-def get_agreement_corpus(): 
+	def show_info(self):
+		self.classifier.show_most_informative_features(n=50)
+
+def build_corpus(binary=False, binary_param=None): 
+	""" 
+	Obtain a corpus, often to be used for training.
+
+	:param binary: boolean so that the corpus uses binary classifications.
+	:param binary_param: string which specifies which category to have, category2 is 'other'.
+
+	Returns a corpus, which is categorized using information from the WiserDatabase.
+	"""
 	import csv
 	import os
 	from helper import WiserDatabase
-
 	wd = WiserDatabase()
-	category_names = wd.get_category_names()
-
 	fileids = list()
 	cats = list()
 	corpus_details = {}
 
-	categories = ['convertible']
-	for category in categories:
+	if (binary) and (binary_param):
+		binary_search = [binary_param]
+	else:
+		binary_search = ['nondisclosure']	
+
+	for category in binary_search:
 		results = wd.fetch_by_category(category)
 		for r in results:
 			if r is not None:
@@ -91,7 +108,7 @@ def get_agreement_corpus():
 				corpus_details[r['filename']] = r['category']
 
 	categories = wd.get_category_names()
-	categories.remove('convertible')
+	categories.remove(binary_search[0])
 	for category in categories:
 		results = wd.fetch_by_category(category)
 		for r in results:
@@ -102,28 +119,61 @@ def get_agreement_corpus():
 
 	print("Loading the training corpus.")
 	newvals = {}
+	binvals = {}
+	i = 0
 	for f in fileids:
-		newvals[f] = corpus_details[f]
+		newvals[f] = [corpus_details[f]]
+		binvals[f] = [cats[i]]
+		i = i + 1
 
-	train_corpus = CategorizedPlaintextCorpusReader(CORPUS_PATH, fileids, cat_map=newvals)
+	if (binary):
+		train_corpus = CategorizedPlaintextCorpusReader(CORPUS_PATH, fileids, cat_map=binvals)
+	else:
+		train_corpus = CategorizedPlaintextCorpusReader(CORPUS_PATH, fileids, cat_map=newvals)
+
 	return train_corpus
 
 def get_agreement_classifier_v1(train_corpus):
+	""" """
 	count_vectorizer = CountVectorizer(input='content', stop_words=None, ngram_range=(1,2))
 	countClass = AgreementVectorClassifier(count_vectorizer, train_corpus)
 	countClass.fit()
 	return countClass
 
 def get_agreement_classifier_v2(train_corpus):
+	""" """
 	tfidf_vectorizer = TfidfVectorizer(input='content', stop_words=None, ngram_range=(1,2))	
 	tfidfClass = AgreementVectorClassifier(tfidf_vectorizer, train_corpus)
 	tfidfClass.fit()
 	return tfidfClass
 
 def get_agreement_classifier_v3(train_corpus):
+	""" """
 	naiveClass = AgreementNaiveBayesClassifier(train_corpus)
 	naiveClass.fit()
 	return naiveClass
+
+def testing():
+	print("Loading the datastores...")
+	from helper import WiserDatabase
+	datastore = WiserDatabase()
+	print("Build a corpus of documents...")
+	corpus = build_corpus(binary=False, binary_param='nondisclosure')
+	print("Loading the agreement classifier...")
+	classifier = get_agreement_classifier_v3(corpus)
+	print("Application ready to load.")
+	#agreement_type = classifier.classify_file("/home/obironkenobi/Projects/ContractWiser/small-data/nda-0000-0025.txt")
+	doc = corpus.words("nda-0000-0025.txt")
+	agreement_type = classifier.classify_data(doc)
+	print("The agreement is a %s agreement" % agreement_type)
+	#classifier.show_info()
+	doc = corpus.words("nda-0000-0001.txt")
+	agreement_type = classifier.classify_data(doc)
+	print("The agreement is a %s agreement" % agreement_type)
+
+	doc = corpus.words("b9debb6714e6c54e0e3ac431af2215c24784d195418988be65568fd92dfb3fd7")
+	agreement_type = classifier.classify_data(doc)
+	print("The agreement is a %s agreement" % agreement_type)
 
 def main():
 	pass
