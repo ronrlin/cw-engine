36439b356447d9f2200cd1d4fa6df4de52e55ae9
Ron Lin <ronrlin@gmail.com>
2015-10-06 15:14:23 -0700

    moved some stuff to config file

---
+++ config.py
@@ -0,0 +1,61 @@
+#!/usr/bin/python
+from helper import WiserDatabase
+
+"""
+Instructions on creating the zip file stored on S3
+%> cd <app root>
+%> tar -czvf data2.tar.gz -C data/ .
+"""
+datasource_url = "https://s3-us-west-2.amazonaws.com/contractwiser-datasource/sample.txt"
+
+# --DATABASE SETTINGS
+# specify the database name
+db_name = "wiser_db"
+# specify the hostname
+db_hostname = 'localhost'
+# specify the port
+db_port = 27017
+# This file specifies the types of contracts
+classifier_source = "./train/master-classifier.csv"
+
+def init():
+	""" Run init to set things up. """
+	print("Installing ContractWiser engine...")
+	print("Fetching datasource from Amazon S3...")
+	import urllib
+	dest_filename = "./datasource.tar.gz"
+	try:
+		name, hdrs = urllib.urlretrieve(datasource_url, dest_filename)
+		print("Archive is saved localled as %s" % name)
+		print(hdrs)
+	except IOError, e:
+		print(e)
+	# Go to AWS and download a zip file of data
+	# Extract the AWS zip file
+	# Get the classified.csv to load the database
+	import zipfile
+	import tarfile
+	try:
+		#change this to be dest_filename
+		if tarfile.is_tarfile("./data.tar.gz"):
+			print("Source file is a tarfile.")
+		else:
+			print("Source file is not a tarfile.")
+			print("exiting with an error")
+			return
+
+		t = tarfile.open("./data.tar.gz")
+		print("Extracting files to ./data ...")
+		t.extractall("./test-data/")
+	except tarfile.TarError, e:
+		print("there was a tarfile problem")
+		return
+
+	# TODO: Should we cp the master-classifier.csv somewhere?
+	print("Create the universe.")
+	print("Database will be called %s." % db_name)
+	datastore = WiserDatabase()
+	datastore.create_universe()
+
+if __name__ == "__main__":
+    pass
